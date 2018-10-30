78a2ea3edcadddce4e745efdaa7ea20cc3f16da4
gngeorgiev <gngeorgiev.it@gmail.com>
2018-10-30 22:40:45 +0200

    initial commit

---
+++ index.ts
@@ -0,0 +1,54 @@
+import { BrowserHeaders } from 'browser-headers';
+
+export type Metadata = BrowserHeaders | { [key: string]: string };
+
+export type ServiceError = {
+    message: string;
+    code: number;
+    metadata: Metadata;
+};
+
+export interface Client {
+    serviceHost: string;
+}
+
+type FirstArgumentType<T> = T extends (arg: infer U) => any ? U : any;
+
+type CallbackArgumentType<T> = T extends (
+    _: any,
+    callback: (_: any, response: infer U) => void
+) => any
+    ? U
+    : any;
+
+export type PromiseClientProxy<T extends Client> = {
+    [P in keyof T]: (
+        req: FirstArgumentType<T[P]>,
+        meta?: Metadata
+    ) => Promise<CallbackArgumentType<T[P]>>
+};
+
+export function PromisifyClient<TClient extends Client>(
+    client: TClient
+): PromiseClientProxy<TClient> {
+    //we don't really care about type safety here
+    //type safety is guaranteed for the user by the proxy type
+    const proxy = Object.keys(Object.getPrototypeOf(client))
+        .filter(k => typeof (client as any)[k] === 'function')
+        .reduce((acc: any, method: string) => {
+            acc[method] = (req: any, meta: any) => {
+                return new Promise((resolve, reject) => {
+                    (client as any)[method].bind(client)(
+                        req,
+                        meta,
+                        (err: ServiceError, resp: any) =>
+                            err ? reject(err) : resolve(resp)
+                    );
+                });
+            };
+
+            return acc;
+        }, {});
+
+    return proxy as PromiseClientProxy<TClient>;
+}
